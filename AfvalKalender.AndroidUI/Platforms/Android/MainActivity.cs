d5e8cb7061685e0c8d6dcd4c547bbfb69b4a5aad
marvinstorage <marvinstorage@users.noreply.github.com>
2026-06-06 21:56:36 +0200

    Fix Android app visibility by adding missing platform files and MainLauncher attribute

---
+++ AfvalKalender.AndroidUI/Platforms/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace AfvalKalender.AndroidUI;
+
+[Activity(Theme = "@style/Maui.MainTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
